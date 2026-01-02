39fc162afb1c7e8504ca36658c5943d28c1f3a25
inotia00 <108592928+inotia00@users.noreply.github.com>
2026-01-02 16:07:49 +0900

    feat: Add `PatchListGenerator`

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -6,8 +6,47 @@ patches {
         description = "Patches template for Morphe"
         source = "git@github.com:MorpheApp/morphe-patches-template.git"
         author = "Morphe"
-        contact = "contact@morphe.software"
+        contact = "na"
         website = "https://morphe.software"
         license = "Additional conditions under GPL section 7 apply: attribution and project name restrictions. See LICENSE file."
+    }
+}
+
+dependencies {
+    // Used by JsonGenerator.
+    implementation(libs.gson)
+}
+
+tasks {
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.PatchListGeneratorKt")
+    }
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesList")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xcontext-receivers")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/MorpheApp/morphe-patches-template")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
 }
